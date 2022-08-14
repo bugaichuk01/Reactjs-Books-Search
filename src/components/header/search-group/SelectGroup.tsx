64c6e6d7897d7eb8aaa6ccbf1495946529dafc79
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-14 19:09:52 +0300

    Refactor SelectGroup.tsx

--- src/components/header/search-group/SelectGroup.tsx
+++ src/components/header/search-group/SelectGroup.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import SearchInput from "../search-input";
 import './SelectGroup.scss';
 import Select from "./select";
-import {useDispatch} from "react-redux";
-import {searchCategory, searchSort} from "../../../store/actions/searchActions";
 import {AnyAction} from "redux";
+import {useActions} from "../../../_hooks/useActions";
 
 const SelectGroup: React.FC = () => {
-    const dispatch = useDispatch();
+    const {searchCategory, searchSort} = useActions();
 
     const categories = ["all", "art", "biography", "computers", "history", "medical", "poetry"]
         .map(category => {
@@ -20,7 +19,7 @@ const SelectGroup: React.FC = () => {
         })
 
     const onChange = (value: string, option: ((value: string) => AnyAction)) => {
-        dispatch(option(value));
+        option(value);
     }
 
     return (
